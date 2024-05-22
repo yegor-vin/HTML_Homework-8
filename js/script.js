55c473089eb9e4d3a9874c5749337ed407e642f0
yegor-vin <egorvinvit@gmail.com>
2024-05-22 11:51:02 +0300

    homework done

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ modalOpenBtn.addEventListener('click', () => {
 modalCloseBtn.addEventListener('click', () => {
     modalWindow.classList.remove('modal--is-open');
 });
+
+const burgerMenuOpenBtn = document.querySelector('.header__burger-btn');
+const burgerMenuCloseBtn = document.querySelector('.burger-menu__close-btn');
+const burgerMenu= document.querySelector('.burger-menu');
+
+
+burgerMenuOpenBtn.addEventListener('click', () => {
+    burgerMenu.classList.add('burger-menu--is-open')
+});
+
+burgerMenuCloseBtn.addEventListener('click', () => {
+    burgerMenu.classList.remove('burger-menu--is-open')
+});
+
+console.log(burgerMenuOpenBtn);
